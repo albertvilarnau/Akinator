338066c3c85776be12a4c83866375a27beb18be5
Albert Vilarnau <avilarnauillana@gmail.com>
2024-10-25 17:00:17 +0200

    Fix

--- src/javaapplication11/JavaApplication11.java
+++ src/javaapplication11/JavaApplication11.java
@@ -1,11 +1,12 @@
 package javaapplication11;
+import java.util.Objects;
 import java.util.Scanner;
 
 
 public class JavaApplication11 {
 
     public static void main(String[] args) {
-        Movie[] movies = new Movie[30];
+        Movie[] movies = new Movie[27];
 
         // Action
         movies[0] = new Movie("The Matrix", "Lana Wachowski, Lilly Wachowski", "USA", "Action", 1999, false, false, false, 136);
@@ -39,7 +40,7 @@ public class JavaApplication11 {
         movies[20] = new Movie("Interstellar", "Christopher Nolan", "USA", "Science Fiction", 2014, false, true, false, 169);
         movies[21] = new Movie("WALL•E", "Andrew Stanton", "USA", "Science Fiction", 2008, true, true, false, 98);
         movies[22] = new Movie("The End of Evangelion", "Hideaki Anno", "Japan", "Science Fiction", 1997, false, false, false, 87);
-        movies[23] = new Movie("Avatar", "James Cameron", "USA", "Science Fiction", 2009, false,false, false, 162);
+        movies[23] = new Movie("Avatar", "James Cameron", "USA", "Science Fiction", 2009, false, false, false, 162);
         movies[24] = new Movie("Dune", "Denis Villeneuve", "USA", "Science Fiction", 2021, false, false, false, 155);
 
         // Romance
@@ -48,37 +49,116 @@ public class JavaApplication11 {
 
         Question[] questions = new Question[7];
 
-        questions[0] = new Question("Your movie was made in ", new Option[]{ new Option("Japan"), new Option("USA"), new Option("Spain"), new Option("Canada")});
+        questions[0] = new Question("Your movie was made in ", new Option[]{new Option("Japan"), new Option("USA"), new Option("Spain"), new Option("Canada")});
         questions[1] = new Question("Your movie genre is ", new Option[]{new Option("Action"), new Option("Comedy"), new Option("Drama"), new Option("Horror"), new Option("Science Fiction"), new Option("Romance")});
         questions[2] = new Question("Your movie was released between", new Option[]{new Option("1990 and 2000"), new Option("2001 and 2010"), new Option("2011 and 2019"), new Option("2020 and 2024")});
         questions[3] = new Question("Your movie is animated", new Option[]{new Option("Base")});
         questions[4] = new Question("Your movie has an Oscar", new Option[]{new Option("Base")});
         questions[5] = new Question("Your movie is in a Saga", new Option[]{new Option("Base")});
-        questions[6] = new Question("Tu pelicula dura entre", new Option[]{new Option("50 and 100 minutes"), new Option("Mas de 100 minutes")});
-
+        questions[6] = new Question("Your movie lasts between", new Option[]{new Option("50 and 100 minutes"), new Option("Mas de 100 minutes")});
 
         boolean finished = false;
-
-        do{
-            int nQuestion = (int)(Math.random() * 7);
-            Scanner scanner = new Scanner(System.in);
-            boolean decision;
-
-            if(questions[nQuestion].getElegible()){
-                int option = (int)(Math.random() * questions[nQuestion].getOptions().length);
-                if(!questions[nQuestion].getOptions()[option].getHasComeOut()){
+        Scanner scanner = new Scanner(System.in);
+        boolean decision;
+        int nQuestion = 0;
+        int nOption = 0;
 
 
-                }
+        do {
+            nQuestion = (int) (Math.random() * 3);
+            nOption = (int) (Math.random() * questions[nQuestion].getOptions().length);
+            System.out.println(questions[nQuestion].getBase_question() + questions[nQuestion].getOptions()[nOption].getText());
+            decision = scanner.nextBoolean();
+
+            if (questions[nQuestion].getElegible()&&!questions[nQuestion].getOptions()[nOption].getHasComeOut()) {
+                questions[nQuestion].getOptions()[nOption].setHasComeOut(true);
+                switch (nQuestion) {
+                    case 0:
+                        for (Movie movie : movies) {
+                            if (Objects.equals(movie.getCountry(), questions[nQuestion].getOptions()[nOption].getText())){
+                                if(!decision){
+                                    movie.setElegible(false);
+                                }
+                            }
+                            if (!Objects.equals(movie.getCountry(), questions[nQuestion].getOptions()[nOption].getText())){
+                                if(decision){
+                                    movie.setElegible(false);
+                                    questions[nQuestion].setElegible(false);
+                                }
+                            }
+                        }
+                        questions[nQuestion].getOptions()[nOption].setHasComeOut(true);
+                        break;
+                    case 1:
+                        for (Movie movie : movies) {
+                            if (Objects.equals(movie.getGenre(), questions[nQuestion].getOptions()[nOption].getText())){
+                                if(!decision){
+                                    movie.setElegible(false);
+                                }
+                            }
+                            if (!Objects.equals(movie.getGenre(), questions[nQuestion].getOptions()[nOption].getText())){
+                                if(decision){
+                                    movie.setElegible(false);
+                                    questions[nQuestion].setElegible(false);
+                                }
+                            }
+                        }
+                        questions[nQuestion].getOptions()[nOption].setHasComeOut(true);
+                        break;
+                    case 2:
+                        int y1 = 0;
+                        int y2 = 0;
+                        if (questions[nQuestion].getOptions()[nOption].getText().equals("1990 and 2000")) {
+                            y1 = 1990;
+                            y2 = 2000;
+                        } else if (questions[nQuestion].getOptions()[nOption].getText().equals("2001 and 2010")) {
+                            y1 = 2001;
+                            y2 = 2010;
+                        } else if (questions[nQuestion].getOptions()[nOption].getText().equals("2011 and 2019")) {
+                            y1 = 2011;
+                            y2 = 2019;
+                        } else if (questions[nQuestion].getOptions()[nOption].getText().equals("2020 and 2024")) {
+                            y1 = 2020;
+                            y2 = 2024;
+                        }
+
+                        for (Movie movie : movies) {
+                            if (y1 <= movie.getYear() && movie.getYear() <= y2){
+                                if(!decision){
+                                    movie.setElegible(false);
+                                }
+                            }
+                            if (y1 > movie.getYear() && movie.getYear() > y2){
+                                if(decision){
+                                    movie.setElegible(false);
+                                    questions[nQuestion].setElegible(false);
+                                }
+                            }
+                        }
+                        questions[nQuestion].getOptions()[nOption].setHasComeOut(true);
+                        break;
+                    case 3:
+
+
+                    }
             }
 
-
-
-        }while(!finished);
-
-
-
-
+            int mCounter = 0;
+            for (Movie movie : movies) {
+                if (movie.getElegible()) {
+                    System.out.println(movie.getName());
+                    mCounter++;
+                }
+            }
+            if (mCounter == 1) {
+                finished = true;
+                for (Movie moviee : movies) {
+                    if (moviee.getElegible()) {
+                        System.out.printf("Your movie is " + moviee.getName());
+                    }
+                }
+            }
+        }while (!finished) ;
 
     }
 }
